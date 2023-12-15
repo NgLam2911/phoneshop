b0e350ab2116087093771d082bfadb281f5eff7e
Ngọc Lam <nglamvn2911@gmail.com>
2023-12-15 23:22:40 +0700

    Simplify this...

--- src/main/java/nhom9/phoneshop/model/dao/UserDao.java
+++ src/main/java/nhom9/phoneshop/model/dao/UserDao.java
@@ -51,14 +51,12 @@ public class UserDao extends BaseDao{
     public void registerCustomer(String customerName, String username, String email, String phoneNumber, String password, String address, int roleID) {
         String sql1 = "INSERT INTO users (Username, Password, RoleID) VALUES (?, ?, ?)";
         String sql2 = "INSERT INTO customers (CustomerName, Username, Email, Phone, Address) VALUES (?, ?, ?, ?, ?)";
-        try {
-            this.connect();
-            var statement1 = this.getConnection().prepareStatement(sql1);
+        try (var connection = getConnection(); var statement1 = connection.prepareStatement(sql1); var statement2 = connection.prepareStatement(sql2);) {
             statement1.setString(1, username);
             statement1.setString(2, password);
             statement1.setInt(3, roleID);
             statement1.executeUpdate();
-            var statement2 = this.getConnection().prepareStatement(sql2);
+
             statement2.setString(1, customerName);
             statement2.setString(2, username);
             statement2.setString(3, email);
@@ -67,8 +65,6 @@ public class UserDao extends BaseDao{
             statement2.executeUpdate();
         } catch (Exception e) {
             throw new RuntimeException("Failed to connect to db", e);
-        } finally {
-            this.close();
         }
     }
 
