72e1deca5f4a12f7701f55c70a7894cb0ee0ea9b
Ngọc Lam <nglamvn2911@gmail.com>
2023-12-17 16:58:14 +0700

    Hehe

--- src/main/java/nhom9/phoneshop/model/dao/ProductDao.java
+++ src/main/java/nhom9/phoneshop/model/dao/ProductDao.java
@@ -164,7 +164,7 @@ public class ProductDao extends BaseDao {
 
     public ArrayList<ProductBean> searchProduct(String keyword) {
         ArrayList<ProductBean> products = new ArrayList<>();
-        String sql = "SELECT * FROM products INNER JOIN manufacturers ON products.ManufacturerID = manufacturers.ManufacturerID WHERE ProductName LIKE ? OR ManufacturerName LIKE ?";
+        String sql = "SELECT * FROM products INNER JOIN manufacturers ON products.ManufacturerID = manufacturers.ManufacturerID WHERE (ProductName LIKE ?) OR (ManufacturerName LIKE ?)";
         try {
             this.connect();
             var statement = this.getConnection().prepareStatement(sql);
